15caa274aa529bd0a63b7b5dd42791702d781b81
Bubsyyy <lubomirlubomirovkn123@gmail.com>
2024-11-10 12:24:21 +0200

    E03 and E04

---
+++ Introduction to Hibernate/src/main/java/ContainsEmployee.java
@@ -0,0 +1,27 @@
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.Scanner;
+
+public class ContainsEmployee {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PU_Name");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+
+        TypedQuery<Long> query = entityManager.createQuery("SELECT COUNT(e) FROM Employee e WHERE CONCAT(e.firstName, ' ',e.lastName) =:givenName", Long.class);
+        query.setParameter("givenName", name);
+
+        Long employeeCount = query.getSingleResult();
+
+        System.out.println(employeeCount == 0 ? "No" : "Yes");
+
+        entityManager.close();
+    }
+}
